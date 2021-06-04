f9d06742642ec220e17d45812d35cd9eff00bba6
victor kigen <vickigen@gmail.com>
2021-06-04 17:22:14 +0300

    added post view class

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+# from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (ListView,DetailView,CreateView,UpdateView,DeleteView,)
+from .models import Post,Hood
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+
+    # def get_queryset(self):
+    #     return Post.objects.filter(hood=self.request.user.profile.hood)
